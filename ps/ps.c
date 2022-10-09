370e4e59d3861bd02cd22ba23aba90697c0b8904
John Doe <johndoe@example.com>
2022-10-09 22:01:29 +0800

    [feat]ps

--- ps/ps.c
+++ ps/ps.c
@@ -7,17 +7,46 @@ int main(void){
     int lines = 0;
     char name[128];
     char value[128];
-    // if((fp=fopen("/proc/1/statuss","r"))==NULL){
-    //     return -1;
-    // }
-    if((fp=fopen("/opt/code/myproject/a.txt","r"))==NULL){
+    char user[128];
+    char pid[128];
+    char cpu[128];
+    char mem[128];
+    char vsz[128];
+    char rss[128];
+    char tty[128];
+    char stat[128];
+    char start[128];
+    char time[128];
+    char command[128];
+    
+    if((fp=fopen("/proc/1/status","r"))==NULL){
         return -1;
     }
+    // if((fp=fopen("/opt/code/myproject/a.txt","r"))==NULL){
+    //     return -1;
+    // }
     printf("USER\tPID\tCPU\tMEM\tVSZ\tRSS\tTTY\tSTAT\tSTART\tTIME\tCOMMAND\n");
+    int line = 0;
     while(fscanf(fp,"%s%s",name,value)==2){
         printf("%s\t%s\n",name,value);
+        if(line++>40){
+            break;
+        }
+        if(strcmp(name,"Name")==0){
+            strcpy(command,value);
+        }else if(strcmp(name,"Pid")==0){
+            strcpy(pid,value);
+        }else if(strcmp(name,"VmSize")==0){
+            strcpy(vsz,value);
+        }
+        else if(strcmp(name,"VmRSS")==0){
+            strcpy(rss,value);
+        }
+        else if(strcmp(name,"State")==0){
+            strcpy(stat,value);
+        }
     }
-
+    printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",user,pid,cpu,mem,vsz,rss,tty,stat,start,time,command);
     return 0;
     
 }
